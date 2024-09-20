eb2bcb4464e20d15dcb87947a03cc550366160e8
Ankush Menat <ankushmenat@gmail.com>
2024-09-20 20:35:38 +0530

    perf: check dword at a time - 183ms

--- count_bits.c
+++ count_bits.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFSIZE 8192
+#define BUFSIZE 1024
 
 int main(int argc, char* argv[]) {
 	FILE* input;
@@ -13,15 +13,15 @@ int main(int argc, char* argv[]) {
 		input = stdin;
 	}
 
-	unsigned char buffer[BUFSIZE];
+	unsigned long long buffer[BUFSIZE];
 	int bytes_read = 0;
 	unsigned long long ones = 0;
 	unsigned long long total_bits = 0;
 
-	while((bytes_read = fread(buffer, 1, sizeof(buffer), input)) != 0) {
+	while((bytes_read = fread(buffer, sizeof(unsigned long long), BUFSIZE, input)) != 0) {
 		for (int i = 0; i < bytes_read; i++) {
-			ones += __builtin_popcount(buffer[i]);
-			total_bits += 8;
+			ones += __builtin_popcountll(buffer[i]);
+			total_bits += 64;
 		}
 	}
 	printf("1/0 ratio: %f%%\n", ones / (double) total_bits * 100);
