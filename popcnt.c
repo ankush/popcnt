ec434a7818da2f313cbf8bc232a6e7083bb704c7
Ankush Menat <ankushmenat@gmail.com>
2024-09-21 19:23:46 +0530

    perf: MT 34ms

--- popcnt.c
+++ popcnt.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,45 +8,83 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define NTHREADS 6
+
+typedef struct {
+    unsigned long long* start;
+    unsigned long long length;
+    unsigned long long result;
+} Work;
+
+void* count_bits(void* work);
+
+
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		printf("Usage: %s inputfile", argv[0]);
-		exit(1);
-	}
+    if (argc != 2) {
+        printf("Usage: %s inputfile", argv[0]);
+        exit(1);
+    }
 
-	int input_fd = open(argv[1], O_RDONLY);
-	struct stat file_stat;
-	fstat(input_fd, &file_stat);
-	size_t file_size = file_stat.st_size;
+    int input_fd = open(argv[1], O_RDONLY);
+    struct stat file_stat;
+    fstat(input_fd, &file_stat);
+    size_t file_size = file_stat.st_size;
 
-	// Read 64 bits ~ 8 bytes at a time
-	unsigned long long* start = (unsigned long long*) mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, input_fd, 0);
+    // Read 64 bits = 8 bytes at a time
+    unsigned long long* start =
+        (unsigned long long*)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, input_fd, 0);
+    unsigned long long* end = start + file_size / 8;
 
-	madvise(start, file_size, MADV_SEQUENTIAL);
-	unsigned long long* iterator = start;
-	unsigned long long* end = start + file_size / 8;
+    madvise(start, file_size, MADV_SEQUENTIAL);
 
-	unsigned long long ones = 0;
-	while (iterator != end) {
-		// with march=native, this translates to single instruction on x86
-		ones += __builtin_popcountll(*iterator);
-		iterator++;
-	}
+    pthread_t tids[NTHREADS];
+    Work work[NTHREADS];
 
-	// Remove edge case from loop for avoiding dependent instructions in tight loop
-	size_t pending_bytes =  file_size - ((end - start) * 8);
-	unsigned char* char_iterator = (unsigned char*) iterator;
-	unsigned char* char_iterator_end = char_iterator + pending_bytes;
+    unsigned long long chunk_size = (end - start) / NTHREADS;
+    for (int i = 0; i < NTHREADS; i++) {
+        work[i].start = start + (chunk_size * i);
+        work[i].length = chunk_size;
+        work[i].result = 0;
+        pthread_create(&tids[i], NULL, count_bits, &work[i]);
+    }
 
-	while (char_iterator != char_iterator_end) {
-		ones += __builtin_popcount(*char_iterator);
-		char_iterator++;
-	}
+    // Remove edge case from loop for avoiding dependent instructions in tight loop
+    unsigned long long ones = 0;
+    size_t pending_bytes = file_size - ((end - start) * 8);
+    unsigned char* char_iterator = (unsigned char*)start + chunk_size * NTHREADS;
+    ;
+    unsigned char* char_iterator_end = char_iterator + pending_bytes;
 
-	munmap(start, file_size);
+    while (char_iterator != char_iterator_end) {
+        ones += __builtin_popcount(*char_iterator);
+        char_iterator++;
+    }
 
-	size_t total_bits = file_size * 8;
-	printf("# of 0s: %llu\n", total_bits - ones);
-	printf("# of 1s: %llu\n", ones);
-	printf("1/0 ratio: %f%%\n", ones / (double) total_bits * 100);
+    for (int i = 0; i < NTHREADS; i++) {
+        pthread_join(tids[i], NULL);
+        ones += work[i].result;
+    }
+    munmap(start, file_size);
+
+    size_t total_bits = file_size * 8;
+    printf("# of 0s: %llu\n", total_bits - ones);
+    printf("# of 1s: %llu\n", ones);
+    printf("1/0 ratio: %f%%\n", ones / (double)total_bits * 100);
+}
+
+
+void* count_bits(void* data) {
+    Work* work = (Work*)data;
+    unsigned long long* iterator = work->start;
+    unsigned long long* end = iterator + work->length;
+    unsigned long long ones = 0;
+
+    while (iterator != end) {
+        // with march=native, this translates to single instruction on x86
+        ones += __builtin_popcountll(*iterator);
+        iterator++;
+    }
+
+    work->result = ones;
+    return NULL;
 }
